4536ef1c045cb7c22427193f1c1aa48a1de4ff52
sebtech974 <sebtech974@gmail.com>
2024-08-12 09:06:50 +0400

    Initial Commit

---
+++ XcodeKumul974/XcodeKumul974App.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeKumul974App.swift
+//  XcodeKumul974
+//
+//  Created by SEBASTIEN BOUILLET on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct XcodeKumul974App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
