35c5cfb9056adfb3e2c6a99323ae57f461a58f0d
sebtech974 <sebtech974@gmail.com>
2024-08-12 15:22:26 +0400

    maj contentView

---
+++ XcodeKumul974/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  XcodeKumul974
+//
+//  Created by SEBASTIEN BOUILLET on 12/08/2024.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
